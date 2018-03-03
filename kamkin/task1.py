3f43390576443c44d16cc591b15514e8dc9fc06a
ddenn132 <kamkin@ccfit.nsu.ru>
2018-03-03 23:22:14 +0700

    Create task1.py

---
+++ kamkin/task1.py
@@ -0,0 +1,15 @@
+def getList(n):
+	array = []
+	number = 2
+
+	while n > 1:
+		count = 0
+		while n % number == 0:
+			n //= number
+			count += 1
+		if count != 0:
+			array += [[number, count]]
+		number += 1
+	return array
+	
+print(getList(51))
