3ad558e59847664a683c5f2c5f2bde0430f8b791
Иван Миляев <milyaev@MacBook-Pro-Ivan.local>
2018-03-11 19:43:01 +0700

    improved directory checking

--- problems-1/milyaev/task-4/task-4.py
+++ problems-1/milyaev/task-4/task-4.py
@@ -3,25 +3,41 @@ import sys
 
 
 def get_files_list(path):
-    if not os.path.isdir(path):
-        raise ValueError(path + ": is not directory")
+    if os.path.isdir(path):
+        if not os.access(path, os.R_OK):
+            raise ValueError("You don't have permission to read this directory.")
+    else:
+        if not os.access(path, os.F_OK):
+            raise ValueError("This directory doesn't exist.")
+        else:
+            raise ValueError("This is not directory.")
 
-    files = {}
+    files = []
 
     for element in os.listdir(path):
         element_path = os.path.join(path, element)
         if os.path.isfile(element_path):
             statinfo = os.stat(element_path)
-            files[element] = statinfo.st_size
+            files.append((element, statinfo.st_size))
 
-    return sorted(files.items(), key=lambda item: item[1], reverse=True)
+    return sorted(files, key=lambda x: (-x[1], x[0]))
 
 
 def main():
     if len(sys.argv) < 2:
-        raise ValueError("Name of directory was not found")
-
-    print(*get_files_list(sys.argv[1]), sep='\n')
+        print("Name of directory was not found.")
+    else:
+        print("path to directory:", sys.argv[1])
+        print()
+        try:
+            file_list = get_files_list(sys.argv[1])
+            if not file_list:
+                print("This directory doesn't contain any regular files.")
+            else:
+                for name, size in file_list:
+                    print("name:", name + ',', "size:", size)
+        except ValueError as e:
+            print(e)
 
 
 if __name__ == "__main__":
