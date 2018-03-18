14747332fab26a03c4eb74a284ce1ef2ffc692cf
Егор Пятаев <e.piataev@g.nsu.ru>
2018-03-18 21:59:47 +0700

    Sorting fixed

--- problems-1/e-pyataev/lab4.py
+++ problems-1/e-pyataev/lab4.py
@@ -2,11 +2,15 @@
 import os, sys
 
 def print_files(path):
-    l= [[x, y] for x in os.listdir(path) for y in [os.stat(os.path.join(path, x)).st_size]]
-    l.sort(key=lambda x: x[0])
-    l.sort(key=lambda x: x[1], reverse=True)
-    for k, v in l:
-        print(k, v)
+    try:
+        l = [[x, y] for x in os.listdir(path) if os.path.isfile(os.path.join(path, x)) for y in [os.stat(os.path.join(path, x)).st_size]]
+        l.sort(key=lambda x: (-x[1], x[0]))
+
+        for k, v in l:
+            print(k, v)
+
+    except Exception as e:
+        print(e, file=sys.stderr)
 
 if(len(sys.argv) > 1):
     print_files(sys.argv[1])
