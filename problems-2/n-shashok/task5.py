ae3b4cf4bf873957ee86a151ea515d7acc64db9e
Наталья Шашок <n.shashok@g.nsu.ru>
2018-03-24 20:21:05 +0700

    новый файл:    problems-2/n-shashok/task5.py

---
+++ problems-2/n-shashok/task5.py
@@ -0,0 +1,69 @@
+import task4
+import sys
+
+encodings = ["cp855", "cp866", "cp1251", "iso8859_5", "koi8-r", "mac_cyrillic"]
+
+frequency = {
+    "о": 10.983,
+    "е": 8.483,
+    "а": 7.998,
+    "и": 7.367,
+    "н": 6.7,
+    "т": 6.318,
+    "с": 5.473,
+    "р": 4.746,
+    "в": 4.533,
+    "л": 4.343,
+    "к": 3.486,
+    "м": 3.203,
+    "д": 2.977,
+    "п": 2.804,
+    "у": 2.615,
+    "я": 2.001,
+    "ы": 1.898,
+    "ь": 1.735,
+    "г": 1.687,
+    "з": 1.687,
+    "б": 1.592,
+    "ч": 1.45,
+    "й": 1.208,
+    "х": 0.966,
+    "ж": 0.94,
+    "ш": 0.718,
+    "ю": 0.638,
+    "ц": 0.486,
+    "щ": 0.361,
+    "э": 0.331,
+    "ф": 0.267,
+    "ъ": 0.037,
+    "ё": 0.013
+}
+
+
+def detect_encoding(filename):
+    bytes_freq = task4.get_bytes(filename)
+    print(bytes_freq)
+    encodings_coefficients = {}
+    for item in bytes_freq:
+        if item[1] < 0.15:
+            continue
+        for encoding in encodings:
+            try:
+                encodings_coefficients[encoding] = \
+                    encodings_coefficients.get(encoding, 0) + \
+                    abs(frequency[(item[0].decode(encoding)).lower()] - item[1])
+            except KeyError:
+                encodings_coefficients[encoding] = \
+                    encodings_coefficients.get(encoding, 0) + item[1]
+    # print(encodings_coefficients)
+    return min(encodings_coefficients, key=encodings_coefficients.get)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: " + __file__ + " directory_name")
+    else:
+        try:
+            print(detect_encoding(sys.argv[1]))
+        except OSError as e:
+            print("Unable to read: ", e, file=sys.stderr)
