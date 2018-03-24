14e9cc461f4ec22616bee7083b810050c9c859c3
Наталья Шашок <n.shashok@g.nsu.ru>
2018-03-24 20:03:09 +0700

    изменено:      problems-1/n-shashok/task4.py

--- problems-1/n-shashok/task4.py
+++ problems-1/n-shashok/task4.py
@@ -3,19 +3,19 @@ import sys
 
 
 def sorted_files(directory):
-    try:
-        files = [os.path.join(os.path.abspath(directory), f)
-             for f in os.listdir(os.path.abspath(directory))
-             if os.path.isfile(os.path.join(os.path.abspath(directory), f))]
-        files.sort(key=lambda x: (os.stat(x).st_size, x.lower()))
-        for file in files:
-            print(file)
-    except OSError as e:
-        print("Unable to read: ", e)
+    files = [os.path.join(os.path.abspath(directory), f)
+         for f in os.listdir(os.path.abspath(directory))
+         if os.path.isfile(os.path.join(os.path.abspath(directory), f))]
+    files.sort(key=lambda x: (os.stat(x).st_size, x.lower()))
+    for file in files:
+        print(file)
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: " + __file__ + " dirname")
     else:
-        sorted_files(sys.argv[1])
+        try:
+            sorted_files(sys.argv[1])
+        except OSError as e:
+            print("Unable to read: ", e, file=sys.stderr)
