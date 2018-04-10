98a1cc130c3385d4ba91e633016009694b5bc3a2
RoyPy <bogush.vano@gmail.com>
2018-04-10 20:27:59 +0700

    Add docstring to file_buffer

--- problems-3/bogush/problem3/file_buffer.py
+++ problems-3/bogush/problem3/file_buffer.py
@@ -2,6 +2,14 @@ from typing import IO
 
 
 def file_buffer(fp: IO, buff_size: int=512):
+    """
+    Returns generator object that reads the specified file fp
+    in portions of size ≤ buff_size and yields each read byte
+    or char (depends on fp.mode).
+
+    :param fp: file object to read from
+    :param buff_size: maximum size of portions
+    """
     while True:
         units = fp.read(buff_size)
         if len(units) == 0:
