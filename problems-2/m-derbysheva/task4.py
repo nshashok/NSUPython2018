a0085eecabe64e8a93ec689b5fa17da773fe3d33
íÁÒÉÑ äÅÒÂÙÛÅ×Á <m.derbysheva@g.nsu.ru>
2018-04-13 22:26:05 +0700

    add reading by parts

--- problems-2/m-derbysheva/task4.py
+++ problems-2/m-derbysheva/task4.py
@@ -1,18 +1,24 @@
 import sys
 
-file = open(sys.argv[1], 'rb')
-data = file.read()
-n = 0
+try:
+    file = open(sys.argv[1], 'rb')
+    n = 0
 
-check = dict.fromkeys(range(128, 256), 0)
+    check = dict.fromkeys(range(128, 256), 0)
+    while True:
+        data = file.read(1024)
+        if len(data) == 0:
+            break
+        for i in data:
+            if i > 127:
+                check[i] += 1
+                n += 1
 
-for i in data:
-    if i > 127:
-        check[i] += 1
-        n += 1
+    for key, counter in sorted(check.items(),
+                               key=lambda element: element[1], reverse=True):
+        if counter > 0:
+            char = bytes([key]).decode('cp1251')
+            print(char + " " + str(counter / n))
 
-for key, counter in sorted(check.items(),
-                           key=lambda element: element[1], reverse=True):
-    if counter > 0:
-        char = bytes([key]).decode('cp1251')
-        print(char + " " + str(counter / n))
+except IndexError as e:
+    print(e, file=sys.stderr)
