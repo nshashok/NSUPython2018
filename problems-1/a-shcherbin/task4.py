d25e06d392007ea05644af343215716f24f71bce
andreysher <30853561+andreysher@users.noreply.github.com>
2018-05-18 17:24:47 +0700

    Create task4.py

---
+++ problems-1/a-shcherbin/task4.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+
+def sorted_list_of_files() -> list:
+    if (len(sys.argv) < 2):
+        print("Enter directory path")
+        return None
+
+    if not os.path.isdir(sys.argv[1]):
+        print("Enter DIRECTORY path")
+        return None
+
+    files = []
+
+    try:
+        inDir = os.listdir(sys.argv[1])
+        for name in inDir:
+            filePath = os.path.join(sys.argv[1], name)
+            if (os.path.isfile(filePath)):
+                files.append((name, os.stat(filePath).st_size))
+
+        files = sorted(files, key=lambda f: (-f[1], f[0]))
+
+        return files
+    except OSError as e:
+        print("Permission denied", e, file=sys.stderr)
+
+
+if __name__ == '__main__':
+    sortedFiles = sorted_list_of_files()
+    print(sortedFiles)
