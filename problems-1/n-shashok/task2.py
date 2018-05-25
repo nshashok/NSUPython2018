8bf8448d24cc4a25ca1a885e9bf7a9230169f076
Shashok Natalia <n.shashok@g.nsu.ru>
2018-05-25 17:38:12 +0700

    Create task2.py

---
+++ problems-1/n-shashok/task2.py
@@ -0,0 +1,18 @@
+import math
+import sys
+
+
+def is_prime(n):
+    for x in range(2, int(math.sqrt(n) + 1)):
+        if n % x == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    while True:
+        try:
+            num = input("Input number: ")
+            print("Is it prime: ", is_prime(int(num)))
+            break
+        except ValueError:
+            print("\tCan't parse value", file=sys.stderr)
